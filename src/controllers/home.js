e516e0b88356363573cf36e20485a3fd074b92fc
feblesariel <feblesariel@gmail.com>
2023-05-15 11:36:18 -0300

    1.13

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -6,13 +6,17 @@ const db = require('../database/models/index.js');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
+const Store = db.Store;
 const User = db.User;
 const Cart = db.Cart;
 const Cart_item = db.Cart_item;
-const Product = db.Product;
 const Category = db.Category;
+const Product = db.Product;
+const Product_image = db.Product_image;
 const Payment = db.Payment;
+const Shipping_method = db.Shipping_method;
 const Shipment = db.Shipment;
+const Store_pickup = db.Store_pickup;
 
 // ************ Controllers ************
 
