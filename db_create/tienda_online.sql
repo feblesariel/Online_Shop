77f67f8d49478d4ce426df3ec50d1bb9ef95217d
feblesariel <feblesariel@gmail.com>
2023-05-16 09:54:31 -0300

    1.32

--- db_create/tienda_online.sql
+++ db_create/tienda_online.sql
@@ -51,15 +51,6 @@ CREATE TABLE product_images (
   FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
--- Creación de la tabla favorites
-CREATE TABLE favorites (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  product_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
-);
-
 -- Creación de la tabla carts
 CREATE TABLE carts (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -13,7 +13,6 @@ const Cart_item = db.Cart_item;
 const Category = db.Category;
 const Product = db.Product;
 const Product_image = db.Product_image;
-const Favorite = db.Favorite;
 const Payment = db.Payment;
 const Shipping_method = db.Shipping_method;
 const Shipment = db.Shipment;
@@ -61,21 +60,11 @@ const homeController = {
             where: { user_id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
           }
         ]
-      });
+      });  
 
-      const getProductCountInFavorite = Favorite.count({
-        include: [
-          {
-            model: User,
-            as: 'user',
-            where: { id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
-          }
-        ]
-      });      
-
-      Promise.all([getFeaturedProducts, getPopularProducts, getCategories, getProductCountInCart, getProductCountInFavorite])
-        .then(([FeaturedProducts, PopularProducts, CategoriesResult, ProductCountInCart, ProductCountInFavorite]) => {
-          res.render('index', { FeaturedProducts, PopularProducts, CategoriesResult, ProductCountInCart, ProductCountInFavorite });
+      Promise.all([getFeaturedProducts, getPopularProducts, getCategories, getProductCountInCart])
+        .then(([FeaturedProducts, PopularProducts, CategoriesResult, ProductCountInCart]) => {
+          res.render('index', { FeaturedProducts, PopularProducts, CategoriesResult, ProductCountInCart});
         })
         .catch(error => {
           console.error('Error:', error);
