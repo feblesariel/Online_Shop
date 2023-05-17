069d1e2f0c04485471ea06abce806f190332f39c
feblesariel <feblesariel@gmail.com>
2023-05-17 07:42:15 -0300

    1.39

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -33,30 +33,62 @@ const productsController = {
         });
 
         // CALCULO CUANTSO ITEMS HAY EN CARRITO
-    
-        const getProductCountInCart = Cart_item.sum( 'quantity' ,{
-        include: [
-            {
-            model: Cart,
-            as: 'cart',
-            where: { user_id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
-            }
-        ]
+
+        const getProductCountInCart = Cart_item.sum('quantity', {
+            include: [
+                {
+                    model: Cart,
+                    as: 'cart',
+                    where: { user_id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
+                }
+            ]
         });
 
-        Promise.all([getCategories, getProductCountInCart])
-        .then(([CategoriesResult, ProductCountInCart]) => {
-            res.render('products', {CategoriesResult,ProductCountInCart});
+
+        const categoriesWithProductCount = Category.findAll({
+            attributes: ['id', 'name'],
+            include: [{
+                model: Product,
+                as: 'products',
+                attributes: [],
+            }],
+            group: ['Category.id', 'Category.name'],
+            having: {
+                '$Category.id$': {
+                    [Op.ne]: null
+                },
+                '$Category.name$': {
+                    [Op.ne]: null
+                },
+                '$products.id$': {
+                    [Op.ne]: null
+                }
+            }
+        }).then(categories => {
+            // Obtener la cantidad de productos por categoría
+            const categoriesWithCount = categories.map(category => ({
+                id: category.id,
+                name: category.name,
+                productCount: category.products.length
+            }));
+            console.log(categoriesWithCount)
         })
-        .catch(error => {
-            console.error('Error:', error);
-            // Manejo de errores
-        });
+
+
+
+        Promise.all([getCategories, getProductCountInCart])
+            .then(([CategoriesResult, ProductCountInCart]) => {
+                res.render('products', { CategoriesResult, ProductCountInCart });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Manejo de errores
+            });
 
     },
 
-    detail: function (req, res) {    
-        
+    detail: function (req, res) {
+
         // CONSULTO PRODUCTO
 
         const getProductByPk = Product.findByPk(req.params.id);
@@ -65,36 +97,36 @@ const productsController = {
 
         const getImagesProduct = Product_image.findAll({
             where: { product_id: req.params.id }
-          })
+        })
 
         // CONSULTO LAS CATEGORIAS
 
         const getCategories = Category.findAll({
             order: [
-              ['name', 'ASC']
+                ['name', 'ASC']
             ]
         });
 
         // CALCULO CUANTSO ITEMS HAY EN CARRITO
-    
-        const getProductCountInCart = Cart_item.sum( 'quantity' ,{
-        include: [
-            {
-            model: Cart,
-            as: 'cart',
-            where: { user_id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
-            }
-        ]
+
+        const getProductCountInCart = Cart_item.sum('quantity', {
+            include: [
+                {
+                    model: Cart,
+                    as: 'cart',
+                    where: { user_id: 1 } // ACA MODIFICAR SEGUN USER LOGUEADO
+                }
+            ]
         });
 
-        Promise.all([getProductByPk, getImagesProduct ,getCategories, getProductCountInCart])
-        .then(([ProductByPk, ImagesProduct ,CategoriesResult, ProductCountInCart]) => {
-            res.render('detail', {ProductByPk,ImagesProduct,CategoriesResult,ProductCountInCart});
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Manejo de errores
-        });
+        Promise.all([getProductByPk, getImagesProduct, getCategories, getProductCountInCart])
+            .then(([ProductByPk, ImagesProduct, CategoriesResult, ProductCountInCart]) => {
+                res.render('detail', { ProductByPk, ImagesProduct, CategoriesResult, ProductCountInCart });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Manejo de errores
+            });
 
     }
 }
