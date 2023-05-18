13c7b0b0b52bd07ecaf59a85bd6bb2f8299b21b9
feblesariel <feblesariel@gmail.com>
2023-05-18 10:31:20 -0300

    1.68

--- src/app.js
+++ src/app.js
@@ -32,22 +32,10 @@ app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la 
 const homeRouter = require("./routes/home");
 const productsRouter = require("./routes/products");
 const searchRouter = require("./routes/search");
-// const usersRouter = require("./routes/users");
-// const categoriesRouter = require("./routes/categories")
-// const cartsRouter = require("./routes/carts")
-// const paymentsRouter = require("./routes/payments");
-// const shipmetsRouter = require("./routes/shipments")
-// const contactRouter = require("./routes/contact")
 
 app.use('/', homeRouter);
 app.use('/products/', productsRouter);
 app.use('/search/', searchRouter);
-// app.use('/users', usersRouter);
-// app.use('/categories',categoriesRouter);
-// app.use('/carts',cartsRouter);
-// app.use('/payments',paymentsRouter);
-// app.use('/shipments',shipmetsRouter);
-// app.use('/contact',contactRouter);
 
 // ************ Run server ************
 
