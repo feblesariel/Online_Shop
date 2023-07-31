ce09c905d23668a8c03cd7810b9c7f4344f1c0a7
feblesariel <feblesariel@gmail.com>
2023-07-31 16:59:16 -0300

    Settings-Products

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -1,5 +1,10 @@
 // ************ Requires ************
 
+const fs = require("fs");
+const path = require("path");
+const { validationResult } = require("express-validator");
+const { stringify } = require("querystring");
+
 //--- DB
 
 const db = require('../database/models/index.js');
@@ -114,6 +119,109 @@ const settingsController = {
       console.error('Error:', error);
       // Manejo de errores
     });
+  },
+
+  productCreate: function (req, res) {
+
+    let user = 0;
+
+    if (req.session.userLogged) {
+      let userLogged = req.session.userLogged;
+      user = userLogged.id;
+    } 
+
+    // consulto las categorias - navbar
+
+    const getCategories = Category.findAll({
+      attributes: [
+        'id',
+        'name',
+        [
+          sequelize.literal('(SELECT COUNT(*) FROM products WHERE products.category_id = Category.id)'),
+          'productCount'
+        ]
+      ],
+      having: sequelize.literal('productCount > 0'),
+      order: [
+        ['name', 'ASC']
+      ],
+      raw: true
+    });
+
+    // calculo cuantos items hay en el carrito - navbar
+
+    const getProductCountInCart = Cart_item.sum('quantity', {
+      include: [
+        {
+          model: Cart,
+          as: 'cart',
+          where: { user_id: user } // ACA MODIFICAR SEGUN USER LOGUEADO
+        }
+      ]
+    });
+
+    // consulto los productos - listado de productos ajustes
+
+    const getProducts = Product.findAll({
+      attributes: [
+        'id',
+        'code',
+        'name',
+        'price',
+        'stock'
+      ],
+      order: [
+        ['code', 'ASC']
+      ],
+      raw: true
+    });
+
+    // consulto los usuarios - listado de usuarios ajustes
+
+    const getUsers = User.findAll({
+      attributes: [
+        'id',
+        'name',
+        'email',
+        'role'
+      ],
+      order: [
+        ['id', 'ASC']
+      ],
+      raw: true
+    });
+
+
+    // creacion de producto y validaciones
+
+
+    let errors = validationResult(req);
+    let defaultImage = "default-image.png";
+
+    if (!errors.isEmpty()) {
+
+
+      Promise.all([getCategories, getProductCountInCart, getProducts ,getUsers])
+      .then(([Categories, ProductCountInCart, Products, Users]) => {
+        res.render('settings', { Categories, ProductCountInCart, Products, Users, errors: errors.array(), old: req.body });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Manejo de errores
+      });
+
+
+    } else {
+
+
+
+
+    }
+
+
+
+
+
   }
 
 }
--- src/routes/settings.js
+++ src/routes/settings.js
@@ -2,8 +2,60 @@
 
 const express = require("express");
 const router = express.Router();
+const multer = require ("multer");
+const path = require ("path");
+const {body} = require("express-validator");
 const routesLoggedRequired = require("../middlewares/routesLoggedRequired");
 const routesAdminRequired = require("../middlewares/routesAdminRequired");
+
+// ************ Multer Config ************
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      let nombreCarpeta = path.join(__dirname, "../../public/img/products");
+      cb(null, nombreCarpeta);
+    },
+    filename: function (req, file, cb) {
+      let nombreImagen = "img-" + Date.now() + path.extname(file.originalname);
+      cb(null, nombreImagen);
+    }
+});
+
+const fileFilter = function (req, file, cb) {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(new Error('Formato de archivo no válido. Solo se permiten imágenes jpg y png.'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 2 * 1024 * 1024,
+    },
+    fileFilter: fileFilter,
+  });
+
+// ************ Validations ************
+
+const validateCategory = (value) => {
+    if (!value || value === "") {
+      throw new Error("Debes seleccionar una categoria.");
+    }
+    return true;
+  };
+
+const validationsCreateProductForm = [
+    body("category").custom(validateCategory),
+    body("newCategory").notEmpty().withMessage("Debes ingresar el nombre de la nueva categoria."),
+    body("code").notEmpty().withMessage("Debes ingresar el codigo."),
+    body("name").notEmpty().withMessage("Debes ingresar el nombre."),
+    body("brand").notEmpty().withMessage("Debes ingresar la marca."),
+    body("model").notEmpty().withMessage("Debes ingresar el modelo."),
+    body("price").notEmpty().withMessage("Debes ingresar el precio.").bail().isNumeric().withMessage("El precio debe ser numérico."),
+    body("stock").notEmpty().withMessage("Debes ingresar el stock.").bail().isNumeric().withMessage("El stock debe ser numérico.")
+];
 
 // ************ Controller Require ************
 
@@ -13,5 +65,7 @@ const settingsController = require ("../controllers/settings")
 
 router.get("/", routesLoggedRequired, routesAdminRequired , settingsController.main);
 router.delete("/user/delete/:id/", routesLoggedRequired, routesAdminRequired , settingsController.userDestroy);
+
+router.post("/product/create/", routesLoggedRequired, routesAdminRequired , upload.array("images", 5) , validationsCreateProductForm, settingsController.productCreate);
 
 module.exports = router;
